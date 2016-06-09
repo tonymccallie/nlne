1aa4854133b4c1fcde94b16a291778029e6df4b4
Tony McCallie <tony@threeleaf.net>
2016-06-09 13:21:23 -0500

    login start

--- www/js/app.js
+++ www/js/app.js
@@ -121,5 +121,5 @@ angular.module('greyback', ['ionic', 'greyback.controllers', 'greyback.services'
 		templateUrl: "templates/users/login.html"
 	});
 	// if none of the above states are matched, use this as the fallback
-	$urlRouterProvider.otherwise('/menu/tabs/home');
+	$urlRouterProvider.otherwise('/login');
 });
