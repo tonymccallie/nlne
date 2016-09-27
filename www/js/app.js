783a9370a13a8fdc7b3fa0a6a8971d12bbac7786
Tony McCallie <tony@threeleaf.net>
2016-09-27 10:57:33 -0500

    keyboard accessories

--- www/js/app.js
+++ www/js/app.js
@@ -25,7 +25,7 @@ angular.module('greyback', ['ionic', 'greyback.controllers', 'greyback.services'
 		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
 		// for form inputs)
 		if (window.cordova && window.cordova.plugins.Keyboard) {
-			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
 			cordova.plugins.Keyboard.disableScroll(true);
 
 		}
